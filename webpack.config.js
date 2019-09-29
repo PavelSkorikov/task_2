824d2e6ba9594732d7ae50eb5b6f7962f14c9915
PavelSkorikov <spg75@yandex.ru>
2019-09-29 21:28:40 +0300

    install and config Webpack

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  devServer: {contentBase: './dist'},
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+            pretty: true
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader']
+          })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader']
+          })
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+
+  plugins: [ 
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      chunks: ['index'],
+      template: './src/index.pug'
+    }),
+    new ExtractTextPlugin({filename: 'style.css'})
+  ]
+}

6e62fbec65329f6baa613da0b8091d2eb07c0f65
PavelSkorikov <spg75@yandex.ru>
2019-10-20 19:59:45 +0300

    refactor and add jQuery in "checkbox" block

---
+++ src/UI_kit/Form-Elements/checkbox-buttons/checkbox/checkbox.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $(".checkbox__select").click(function(){
+        $(this).toggleClass("checkbox__select_checked");
+    });
+});
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "./style.scss";
 import "./UI_kit/Form-Elements/radio-buttons/radio-button/radio-button"
+import "./UI_kit/Form-Elements/checkbox-buttons/checkbox/checkbox"
 console.log("hello");

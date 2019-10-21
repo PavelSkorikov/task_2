7f78a3815bfd09d684f2ce2d72cf675c900b7b20
PavelSkorikov <spg75@yandex.ru>
2019-10-21 21:42:28 +0300

    finished the "toggle" block

---
+++ src/UI_kit/Form-Elements/toggle-buttons/toggle/toggle.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $(".toggle__select").click(function(){
+        $(this).toggleClass("toggle__select_checked");
+    });
+});
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.scss";
 import "./UI_kit/Form-Elements/radio-buttons/radio-button/radio-button"
 import "./UI_kit/Form-Elements/checkbox-buttons/checkbox/checkbox"
+import "./UI_kit/Form-Elements/toggle-buttons/toggle/toggle"
 console.log("hello");

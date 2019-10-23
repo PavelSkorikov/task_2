6dc16d02629c1bf5f4a460afaba8482455d2c798
PavelSkorikov <spg75@yandex.ru>
2019-10-23 22:19:00 +0300

    work on "rate button" block

---
+++ src/UI_kit/Form-Elements/rate-button/rate/rate.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".rate__star").click(function(){
+            $(this).text("star_border");
+        
+        
+        //$(this).prevAll(".rate__star").text("star");
+        //$(this).nextAll(".rate__star").text("star_border");
+    });
+});
--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import "./UI_kit/Form-Elements/radio-buttons/radio-button/radio-button"
 import "./UI_kit/Form-Elements/checkbox-buttons/checkbox/checkbox"
 import "./UI_kit/Form-Elements/toggle-buttons/toggle/toggle"
 import "./UI_kit/Form-Elements/like-button/like/like"
+import "./UI_kit/Form-Elements/rate-button/rate/rate"
 console.log("hello");

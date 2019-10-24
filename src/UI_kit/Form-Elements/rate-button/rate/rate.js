a2a2903fc87197c77cd2b6d50f7526cf0fb7a3a0
PavelSkorikov <spg75@yandex.ru>
2019-10-24 22:23:39 +0300

    finished "rate button" block

--- src/UI_kit/Form-Elements/rate-button/rate/rate.js
+++ src/UI_kit/Form-Elements/rate-button/rate/rate.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
     $(".rate__star").click(function(){
-            $(this).text("star_border");
-        
-        
-        //$(this).prevAll(".rate__star").text("star");
-        //$(this).nextAll(".rate__star").text("star_border");
+        if($(this).text() == 'star'){
+             $(this).text('star_border'); //меняем элемент
+             $(this).toggleClass('rate__star_checked'); //обновляем фон элемента для правильного отображения
+        }
+        else {
+            $(this).text('star');//меняем элемент
+            $(this).toggleClass('rate__star_checked');//обновляем фон элемента для правильного отображения
+        }
+        $(this).nextAll('.rate__star').text('star_border');
+        $(this).nextAll('.rate__star').toggleClass('rate__star_checked');
+        $(this).prevAll('.rate__star').text('star');
+        $(this).prevAll('.rate__star').toggleClass('rate__star_checked');
+       
+       
     });
 });

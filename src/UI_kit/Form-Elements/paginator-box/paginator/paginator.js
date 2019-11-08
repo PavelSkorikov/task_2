9dc2bee613a2bbd4ed1aab0c34f8d3b1645e662a
PavelSkorikov <spg75@yandex.ru>
2019-11-08 21:34:47 +0300

    refactor block "arrow" and work on an block "paginator"

--- src/UI_kit/Form-Elements/paginator-box/paginator/paginator.js
+++ src/UI_kit/Form-Elements/paginator-box/paginator/paginator.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-    
-    $('.paginator').initPaginator ({
-        'totalPage': 15,
-        'previousPage': 'Next',
-        'nextPage': 'Previous'
-
+    $('.paginator__page-number').click(function(){{
+        if(($(this).text()!='...')&&(!$(this).hasClass("paginator__page-number_active"))&&(!$(this).children().hasClass("arrow"))){
+            $(this).addClass('paginator__page-number_active')
+        }
+    }
     });
 });
